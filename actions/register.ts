f06afb6016b675e6b6c1906d414e4b770131536a
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2025-02-18 20:40:55 +0600

    prisma create with neon database and first time user create successfully

--- actions/register.ts
+++ actions/register.ts
@@ -2,11 +2,34 @@
 
 import { RegisterSchema } from "@/schemas"
 import { z } from "zod"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db"
 
 export const register =async(values:z.infer<typeof RegisterSchema>) =>{
     const validatedFields = RegisterSchema.safeParse(values);
 
     if(!validatedFields.success) return {error:"Invalid fields!"}
 
-    return {success:"Register Success!"}
+    const {name,email,password} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const existingUser = await db.user.findUnique({
+        where:{email}
+    })
+
+    if(existingUser){
+        return {error:"Email Already in use!"}
+    }
+
+    await db.user.create({
+        data:{
+            name,
+            email,
+            password:hashedPassword
+        }
+    })
+
+    //TODO: SEND verification email
+
+    return {success:"User Created!"}
 }
---
+++ prisma/migrations/20250218131943_add_account/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "email" TEXT,
+ADD COLUMN     "emailVerified" TIMESTAMP(3),
+ADD COLUMN     "image" TEXT,
+ALTER COLUMN "name" DROP NOT NULL;
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "provider" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "expires_at" INTEGER,
+    "token_type" TEXT,
+    "scope" TEXT,
+    "id_token" TEXT,
+    "session_state" TEXT,
+
+    CONSTRAINT "accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts_provider_provider_account_id_key" ON "accounts"("provider", "provider_account_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
