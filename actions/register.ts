85aeefc07f00b5d66c0602b7fb70d6fd2eafbf03
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2025-02-23 17:32:26 +0600

    after registration email send successfully (without setup resend domain, only i get the mail, no one get any email , so pls add a domain)

--- actions/register.ts
+++ actions/register.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs"
 import { db } from "@/lib/db"
 import { getUserByEmail } from "@/data/user"
 import {generateVerificationToken} from "@/lib/token"
+import { sendVerificationEmail } from "@/lib/mail"
 
 export const register =async(values:z.infer<typeof RegisterSchema>) =>{
     const validatedFields = RegisterSchema.safeParse(values);
@@ -31,6 +32,7 @@ export const register =async(values:z.infer<typeof RegisterSchema>) =>{
 
     const verificationToken = await generateVerificationToken(email)
     //TODO: SEND verification email
+    await sendVerificationEmail(verificationToken.email,verificationToken.token)
 
 
     return {success:"Confirmation Email Sent!"}
